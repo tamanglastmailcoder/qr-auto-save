c332b09f6cf009e62d8314456c2f57385d986681
tamanglastmailcoder <tamanglastmail@gmail.com>
2026-02-25 12:14:08 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,16 @@
-self.addEventListener('install', e => {
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open('qr-app-cache').then(cache => 
+    caches.open("qr-app-cache-v2").then((cache) =>
       cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        'https://unpkg.com/html5-qrcode'
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "https://unpkg.com/html5-qrcode"
       ])
     )
   );
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
-  );
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
